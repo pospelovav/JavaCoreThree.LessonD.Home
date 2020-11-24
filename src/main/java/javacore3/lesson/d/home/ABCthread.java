14fee5ae20ea7d4c1f7a08e4807d0eeae8db6da1
pospelovav <shnurokvtaige@yandex.ru>
2020-11-24 22:50:16 +0300

    public class ABCthread  - задание 1 Main.java - delete MFU - create

--- src/main/java/javacore3/lesson/d/home/ABCthread.java
+++ src/main/java/javacore3/lesson/d/home/ABCthread.java
@@ -1,43 +1,80 @@
 package javacore3.lesson.d.home;
+/*
+1. Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз (порядок – ABСABСABС). Используйте wait/notify/notifyAll.
+ */
 
 public class ABCthread {
-    private char printCharacter = 'A'; //для гарантии очередности букв
+    private static char printCharacter = 'A'; //для гарантии очередности букв
 
-    public synchronized void printA() {
-        try {
-            while (printCharacter != 'A') {
-                wait();
-            }
-            System.out.print("A");
-            printCharacter = 'B';
-            notifyAll();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+    public static void main (String[] args) {
+//Задание 1
+        Object monitor = new Object();
 
-    public synchronized void printB() {
-        try {
-            while (printCharacter != 'B') {
-                wait();
+        Thread threadA = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (monitor) {
+                    for (int i = 0; i < 5; i++) {
+                        try {
+                            while (printCharacter != 'A') {
+                               monitor.wait();
+                            }
+                            System.out.print("A");
+                            printCharacter = 'B';
+                            monitor.notifyAll();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }
             }
-            System.out.print("B");
-            printCharacter = 'C';
-            notifyAll();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-    public synchronized void printC() {
-        try {
-            while (printCharacter != 'C') {
-                wait();
+        });
+
+        Thread threadB = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (monitor) {
+                    for (int i = 0; i < 5; i++) {
+                        try {
+                            while (printCharacter != 'B') {
+                                 monitor.wait();
+                            }
+                            System.out.print("B");
+                            printCharacter = 'C';
+                            monitor.notifyAll();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }
             }
-            System.out.print("C");
-            printCharacter = 'A';
-            notifyAll();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        });
+
+        Thread threadC = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (monitor) {
+                    for (int i = 0; i < 5; i++) {
+                        try {
+                            while (printCharacter != 'C') {
+                                monitor.wait();
+                            }
+                            System.out.print("C");
+                            printCharacter = 'A';
+                            monitor.notifyAll();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+        threadC.start();
     }
 }
---
+++ src/main/java/javacore3/lesson/d/home/MFU.java
@@ -0,0 +1,6 @@
+package javacore3.lesson.d.home;
+/*
+2. Создать модель MFU (c возможность сканирования, печати и ксерокопии)
+ */
+public class MFU {
+}
